1b4baa6ca4cb9b201c304c3bddc5cca3028607db
Kieran Walker <38079164+kieran-walker-0@users.noreply.github.com>
2019-02-18 02:09:29 +0000

    Update hibp.py

--- hibp.py
+++ hibp.py
@@ -5,14 +5,13 @@ HIBP API script made in Python3:
  + Check any number of Email addresses for a history of breaches.
  + Check any number of passwords for a history of breaches.
 """
-import urllib.request, json# Imports relevant modules
-#from io import StringIO
+import urllib.request, json, datetime, hashlib, re# Imports relevant modules
 
 # User Agent:
 agent = "HIBP-py"
 
 # Breach API URLs: 
-e_url = "https://haveibeenpwned.com/api/v2/breachedaccount/"# See "https://haveibeenpwned.com/API/v2#UserAgent", TODO: Figure out how to get around it.
+e_url = "https://haveibeenpwned.com/api/v2/breachedaccount/"
 p_url = "https://api.pwnedpasswords.com/range/"
 """
 ^^^ This confused me for a while, it only accepts the first five characters of the hash.
@@ -24,18 +23,15 @@ p_url = "https://api.pwnedpasswords.com/range/"
         3. Search for "1e4c9b93f3f0682250b6cf8331b7ee68fd8" in the response.
         4. If it exists, the password was breached - the number after the colon is the number of times it was breached.
 """
-
-# POTENTIALLY UNUSED
-#psbdmp_domain_url = "https://psbdmp.ws/api/search/domain/"
-#psbdmp_email_url = "https://psbdmp.ws/api/search/email/"
-#psbdmp_string_url = "https://psbdmp.ws/api/search/"
-
 def oc(opt, vs):# Opt Check
 	v_main = {"1","2","3","4","98","99"}# Set of valid opts during main.
+	v_pass = {"1", "2"}# Set of valid opts during password opts.
 	if vs == 0:
 		print("Something has gone wrong, vs=0 is only for testing.")
 	if vs == 1:
 		vset = v_main
+	if vs == 2:
+		vset = v_pass
 	oc_ = opt in v_main # Checks if the value of opt is present in the present in the relevant set.
 	if oc_ == True:
 		pass
@@ -77,7 +73,7 @@ def i():# Interactive interface
 	if opt == "2":
 		print("This is the option for a list of Email addresses")
 	if opt == "3":
-		print("This is the option for the password")
+		sp()
 	if opt == "4":
 		print("This is the option for a list of passwords")
 	if opt == "98":
@@ -90,14 +86,100 @@ def i():# Interactive interface
 ##########################################################################
 # START of single email address portion
 def se():# Single Email
-	pb()
 	e = input("HIBP>Enter an Email address> ")
 	print("Querying the HIBP database with %s..."% e)
 	r = urllib.request.Request(e_url+e, data=None, headers={'User-Agent' : agent})
-	o = urllib.request.urlopen(r)
-	jl = json.loads(o.read().decode('utf-8'))# Loads and decodes JSON response.
-	
+	try:
+		o = urllib.request.urlopen(r)# urllib throws an exception if a 404 is returned here.
+		print("[!] You've been breached!")
+		jl = json.loads(o.read().decode('utf-8'))# Decodes JSON response.
+		jl_num = 0
+		title = []
+		date = []
+		data = []
+		while True:
+			try:
+				jl_index = jl[jl_num]
+				title.append(jl_index["Title"])
+				date.append(jl_index["BreachDate"])
+				data.append(jl_index["DataClasses"])
+				jl_num += 1
+			except:
+				break
+##########################################################
+# FILE INPUT START
+		
+		f_titles = "Breached sites: \n"
+		for t in title:
+			f_titles += ("[+] %s \n"% t)
+		f_data = "\n The following data has been discovered in the above databases: \n "
+		for d in jl_index["DataClasses"]:
+				try:
+					f_data += ("[+] %s \n"% str(d))
+				except:
+					break
+		f_in = """
+HIBP.py
+ Breach data retrieved from 'HaveIBeenPwned?' at """+str(datetime.datetime.now())+""":
+ 
+"""+f_titles+"""
+"""+f_data+"""
+
+
+		 
+		"""
+# FILE INPUT END
+##########################################################################
+		print(f_in)
+	except:# If/when 404 response code is thrown.
+		print("[+] No breach data found, lucky you!")
 # END of single email address portion
+##########################################################################
+# START of single password portion
+def sp():# Single password
+	print("""
+In order to protect the privacy of user-submitted passwords, the PwnedPasswords
+API will allow for search ranges only, this is done by hashing user input using the
+SHA-1 algorithm and showing all hashes that match the first 5 characters (prefix).
+This script will hash any cleartext input and automatically match the prefix with the
+rest of the password's hash.
+
+Of course, in the interest of security, this part of the script should be performed
+under a secured environment. Be aware of physical, software-based and network-based 
+monitoring technologies that may be in place.
+
+This script will not decrypt or store any hashes or cleartext passwords submitted.
+
+Choose an option:
+	[1] Cleartext password
+	[2] SHA-1 Hash
+	
+	""")
+	opt = str(input("HIBP>Cleartext or SHA-1 Hash?> "))
+	oc(opt, 2)
+	if opt == "1":
+		p = input("HIBP>Cleartext password> ")# TODO: Make the user input invisible
+		p = str(p).encode('utf-8')
+		h = hashlib.sha1(p).hexdigest()
+		prefix = h[0:5].upper()
+		suffix = h[5:]
+		r = urllib.request.Request(p_url+prefix, data=None, headers={'User-Agent' : agent})
+		try:
+			o = urllib.request.urlopen(r).read()# Shouldn't be any 404 here, as custom agent is not required.
+			re_compile = re.compile("("+suffix+")\:([0-9])*", flags=re.IGNORECASE)
+			result = re.search(re_compile, str(o))
+			response = result[0]
+			splitter = response.split(":")
+			#split_h = splitter[0]
+			split_n = splitter[1]
+			print("[!] That password has been previously compromised %s times!"% split_n)
+		except:
+			print("[+] No password hash found, lucky you!")
+	if opt == "2":
+		print("HASH")
+
+
+# END of single password portion
 ##########################################################################
 
 
